baa285be53a1acffba90465e6f8634d9749f4190
vjf2 <foroughirad@gmail.com>
2021-03-15 12:35:38 -0400

    fix new axes orientation

--- flip_scale_points.R
+++ flip_scale_points.R
@@ -3,17 +3,22 @@ library(raster)
 ir <- raster("IR_dorsal transparent.png")
 
 load("tpoints.RData")
+
 tp <- coordinates(tpoints)
 
 #need to convert tpoints axes to image axes
 
-#tpoints x 912:0, y 608:0
+#tpoints x 0:912, y 608:0
 #image x 0:480, y 0:284
 
-rescale <- function(x, oldmax, newmax) {newmax - ((x-0)/(oldmax - 0) * newmax) } 
+rescale_x <- function(x, oldmax, newmax) {((x-0)/(oldmax - 0) * newmax)} 
 
-x2 <- rescale(tp[,1], 912, 480)
-y2 <- rescale(tp[,2], 608, 284)
+rescale_y <- function(x, oldmax, newmax) {newmax - ((x-0)/(oldmax - 0) * newmax) } 
 
+x2 <- rescale_x(tp[,1], 912, 480)
+y2 <- rescale_y(tp[,2], 608, 284)
+
+windows()
 plot(ir)
 points(x2, y2)
+points(x2[260:262], y2[260:262], col="red", pch=16) #anchor points
